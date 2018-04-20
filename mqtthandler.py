1f58731fefd1403dcf2062dc5edd873c17c3e87e
Kristian Fjelde Pedersen <krfjpe@gmail.com>
2018-04-20 17:42:06 +0200

    updatestatus completed. db conn and mqtt works perfect

--- mqtthandler.py
+++ mqtthandler.py
@@ -4,14 +4,13 @@ import json
 import _mysql
 import _mysql_exceptions
 
-
 BROKER = "test.mosquitto.org"
 PORT = 1883
 
-DBHOST = ""
-DBUSER = ""
-DBPASS = ""
-DBNAME = ""
+DBHOST = "mysql.stud.ntnu.no"
+DBUSER = "kristiap_test"
+DBPASS = "ttm411515"
+DBNAME = "kristiap_ttm4115"
 
 # TODO: Achieve db connection  [X]
 # TODO: Solve "_mysql.connection' object has no attribute 'cursor"
@@ -24,14 +23,22 @@ class mqttHandler:
 
     def on_message(self, client, userdata, msg):
         print("Received message from topic {}".format(msg.topic))
-        if msg.topic == "ttm4115/15/server/fetchs":
-            dbHandler.fetchhisotry(msg.payload)
+        if msg.topic == "ttm4115/15/server/fetch":
+            dbHandler.fetchhistory(self.d, msg.payload)
+
         elif msg.topic == "ttm4115/15/server/routeplanner":
-            dbHandler.fetchfull(msg.payload)
+            dbHandler.routeplanner(self.d, msg.payload)
+
         elif msg.topic == "ttm4115/15/server/update":
-            dbHandler.updatestatus(msg.payload)
+             res = dbHandler.updatestatus(self.d, msg.payload)
+             if res == "err":
+                 self.client.publish("ttm4115/15/hardware/", "Invalid format")
+
         elif msg.topic == "ttm4115/15/server/register":
-            dbHandler.registerUser(msg.payload)
+            res = dbHandler.register(self.d, msg.payload)
+            if res == "err":
+                self.client.publish("ttm4115/15/workstation", "Invalid format")
+
         else:
             print("Publish to topic not handled: {}".format(msg.topic))
 
@@ -50,6 +57,8 @@ class mqttHandler:
         self.client.subscribe("ttm4115/15/server/#")
         print("Successfully connected to Broker")
 
+        self.d = dbHandler()
+
         #thread = Thread(target=self.client.loop_forever())
         #thread.start()
         #thread.join()
@@ -66,44 +75,56 @@ class dbHandler:
         data = json.loads(payload)
         try:
             for element in data['adresses']:
-                self.cur.execute("SELECT * FROM users")
-                self.cur.fetchall()
+                self.db.query("SELECT * FROM users")
+                self.db.fetchall()
 
         except UnboundLocalError:
             print("'adresses' not found in payload")
 
+    def routeplanner(self, payload):
+        data = json.loads(payload)
+        print("Routeplanner activated")
 
 
     def register(self, payload):
         data = json.loads(payload)
 
         for element in data['adresses']:
-            self.cur.execute("SELECT * FROM adresses")
-            receive = self.cur.fetchall()
+            self.db.query("SELECT * FROM adresses")
+            receive = self.db.fetchall()
 
         print("'adresses' not found in payload")
 
-        json_data = json.dumps(recv)
+        json_data = json.dumps(data)
         mqttHandler.send_message("ttm4115/15/workstation/server", json_data)
 
 
     def updatestatus(self, payload):
         data = json.loads(payload)
-        try:
-            for element in data['adresses']:
-                self.cur.execute("INSERT")
-                self.cur.fetchall()
+        if len(data) == 2 and data[0][:3] == "bin":
+            formatted_string = """INSERT INTO bins (bin_id, amount, time) VALUES ('{0}', {1}, NOW());""".format(data[0], data[1])
+            print(formatted_string)
+            try:
+                self.db.query(formatted_string)
+                self.r = self.db.store_result()
 
-        except UnboundLocalError:
-            print("Unable to execute")
+            except (_mysql_exceptions.MySQLError, _mysql_exceptions.DataError) as err:
+                print("Failed to execute due to {}".format(err))
 
+            if self.r != None:
+                print("Results: ")
+                print(self.r.fetch_row())
+
+            return "Success"
+        else:
+            return "err"
 
     def __init__(self):
 
         try:
             self.db = _mysql.connect(DBHOST, DBUSER, DBPASS, DBNAME)
             print("Connected successfully")
-            self.db.close()
+
         except (_mysql_exceptions.OperationalError, NameError) as err:
             print("Error in db connection: {0}".format(err))
             exit(1)
@@ -114,16 +135,11 @@ class dbHandler:
 
 
 if __name__ == '__main__':
-    dbHandler()
 
     mqttHandler = mqttHandler()
     client = mqttHandler.get_client()
     thread = Thread(target=client.loop_forever())
     thread.start()
-
-
-    client.publish("ttm4115/15/server/ls", "jfhjas", 1)
-    print("Sent to ttm4115/15/server/ls")
 
     thread.join()
     print("Thread finished. Exiting...")
